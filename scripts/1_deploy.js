d496b93cc3555220ca811623a6cd5d356e039eea
Okuich <ok2abella@gmail.com>
2022-10-20 20:10:42 -0500

    2.Smoke test

---
+++ scripts/1_deploy.js
@@ -0,0 +1,19 @@
+const { ethers } = require("hardhat");
+const hre = require("hardhat");
+
+async function main() {
+  // fetch contract to deploy
+  const Token = await ethers.getContractFactory("Token")
+
+  // deploy contract
+  const token = await Token.deploy()
+  await token.deployed()
+  console.log(`Token Deployed to: ${token.address}`)
+
+
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
